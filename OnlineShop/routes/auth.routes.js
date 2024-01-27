ef85082d1832941ff738024f3e3ebd71df541e5a
Mehmet Emin Dağ <71263444+memind@users.noreply.github.com>
2024-01-27 08:24:50 +0300

    Delete OnlineShop directory

--- OnlineShop/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/auth.controller');
-
-router.get('/signup', authController.getSignUp);
-router.post('/signup', authController.signup);
-router.get('/login', authController.getLogin);
-
-module.exports = router;
